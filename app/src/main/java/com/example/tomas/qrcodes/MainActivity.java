924b24aeca6dc304f61735d18db2002492569e62
Tom Snuverink <tom_snuv@hotmail.com>
2016-04-11 11:21:37 +0200

    Added QR code scanner

--- app/src/main/java/com/example/tomas/qrcodes/MainActivity.java
+++ app/src/main/java/com/example/tomas/qrcodes/MainActivity.java
@@ -3,11 +3,22 @@ package com.example.tomas.qrcodes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setPrompt("Scan a barcode");
+        integrator.setCameraId(0);  // Use a specific camera of the device
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(true);
+        integrator.initiateScan();
+
     }
 }
